d55b367f8c84edfb334dd4f16e1e9ecb34ed16c3
Stephan Rayner <stephan.rayner@gmail.com>
2018-03-10 19:19:20 -0800

    Load script is now less ugly

--- loader/load_script.py
+++ loader/load_script.py
@@ -4,33 +4,21 @@ import Queues
 import DB
 
 
-def get_count(channel, queue):
-    method_frame, _, _ = channel.basic_get(queue)
-    if method_frame:
-        return method_frame.message_count
-    else:
-        return 0
-
-
-def load_crash_report_events():
-    queue = 'crash_report'
-    print("Loading::{}".format(queue))
-    mapper = lambda payload: (payload['user_id'], payload['timestamp'], payload['message'])
+def unpack_data(channel, queue, mapper):
+    print("Loading::{}".format(queue), end=' | ')
     data = []
-    # Connect Queue and stuff
-    connection, channel = Queues.queues[queue]
-
     # get count
     count = 0
     method_frame, header_frame, body = channel.basic_get(queue)
     if method_frame:
         count = method_frame.message_count
+        print("Count:", count + 1)
         payload = json.loads(body)
         data.append(mapper(payload))
         channel.basic_ack(method_frame.delivery_tag)
+    else:
+        print('No data to unpack')
 
-    print("Count:", count)
-    
     for i in range(count):
         method_frame, header_frame, body = channel.basic_get(queue)
         if method_frame:
@@ -40,10 +28,16 @@ def load_crash_report_events():
         else:
             print('No message returned')
 
-    # Connect DB stuff
-    conn, curs = DB.get_db()
+    return data
 
+def load_crash_report_events():
+    queue = 'crash_report'
+    mapper = lambda payload: (payload['user_id'], payload['timestamp'], payload['message'])
+    connection, channel = Queues.queues[queue]
+    conn, curs = DB.get_db()
+    data = unpack_data(channel, queue, mapper)
     args_str = b','.join(curs.mogrify("(%s,%s,%s)", x) for x in data)
+    
     if args_str:
         curs.execute(b"INSERT INTO crash_report(user_id, timestamp, message) VALUES " + args_str)
         conn.commit()
@@ -52,35 +46,10 @@ def load_crash_report_events():
 
 def load_purchase_events():
     queue = 'purchase'
-    print("Loading::{}".format(queue))
     mapper = lambda payload: (payload['user_id'], payload['timestamp'], payload['sku'])
-    data = []
-    # Connect Queue and stuff
     connection, channel = Queues.queues[queue]
-
-    # get count
-    count = 0
-    method_frame, header_frame, body = channel.basic_get(queue)
-    if method_frame:
-        count = method_frame.message_count
-        payload = json.loads(body)
-        data.append(mapper(payload))
-        channel.basic_ack(method_frame.delivery_tag)
-
-    print("Count:", count)
-    
-    for i in range(count):
-        method_frame, header_frame, body = channel.basic_get(queue)
-        if method_frame:
-            payload = json.loads(body)
-            data.append(mapper(payload))
-            channel.basic_ack(method_frame.delivery_tag)
-        else:
-            print('No message returned')
-
-    # Connect DB stuff
     conn, curs = DB.get_db()
-
+    data = unpack_data(channel, queue, mapper)
     args_str = b','.join(curs.mogrify("(%s,%s,%s)", x) for x in data)
 
     if args_str:
@@ -94,35 +63,10 @@ def load_purchase_events():
 
 def load_install_events():
     queue = 'install'
-    print("Loading::{}".format(queue))
     mapper = lambda payload: ((payload['user_id'], payload['timestamp']))
-    data = []
-    # Connect Queue and stuff
     connection, channel = Queues.queues[queue]
-
-    # get count
-    count = 0
-    method_frame, header_frame, body = channel.basic_get(queue)
-    if method_frame:
-        count = method_frame.message_count
-        payload = json.loads(body)
-        data.append(mapper(payload))
-        channel.basic_ack(method_frame.delivery_tag)
-
-    print("Count:", count)
-    
-    for i in range(count):
-        method_frame, header_frame, body = channel.basic_get(queue)
-        if method_frame:
-            payload = json.loads(body)
-            data.append(mapper(payload))
-            channel.basic_ack(method_frame.delivery_tag)
-        else:
-            print('No message returned')
-
-    # Connect DB stuff
     conn, curs = DB.get_db()
-
+    data = unpack_data(channel, queue, mapper)
     args_str = b','.join(curs.mogrify("(%s,%s)", x) for x in data)
 
     if args_str:
