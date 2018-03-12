de4e6e630986545cae2ac740fd6a3eb357b1b71f
Stephan Rayner <stephan.rayner@gmail.com>
2018-03-11 21:58:43 -0700

    Added top level exception handler.

--- web/src/__init__.py
+++ web/src/__init__.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# from src.module_name.routes import mod
-# app.register_blueprint(module_name.routes.mod)
+# Top Level Exception Handler
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    return jsonify({
+        'status_code': 500,
+        'status_message': str(e),
+    }), 500
 
 from src.extractor.routes import mod
 app.register_blueprint(extractor.routes.mod)
