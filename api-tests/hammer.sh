b7972c8b4cb9402a69a842eee65c1ceda46c5c02
Stephan Rayner <stephan.rayner@gmail.com>
2018-03-11 18:10:04 -0700

    A few dozen of these in parallel is a wild time.

---
+++ api-tests/hammer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+
+submit_crash_report(){
+    curl -X POST \
+    http://159.65.76.76/ \
+    -H 'cache-control: no-cache' \
+    -H 'content-type: application/json' \
+    -d '{
+        
+        "event_name": "crash_report",
+        "user_id": 1,
+        "timestamp": 1483228810,
+        "message": "Stuff went really well!"
+    }'
+}
+
+submit_purchase(){
+    curl -X POST \
+    http://159.65.76.76/ \
+    -H 'cache-control: no-cache' \
+    -H 'content-type: application/json' \
+    -d '{
+        
+        "event_name": "purchase",
+        "user_id": 1,
+        "timestamp": 1483228810,
+        "sku": "sku-456"
+    }'
+}
+
+submit_install(){
+    curl -X POST \
+    http://159.65.76.76/ \
+    -H 'cache-control: no-cache' \
+    -H 'content-type: application/json' \
+    -H 'postman-token: 72491d61-e7e6-237e-e46c-f9180e71104b' \
+    -d '{
+        
+        "event_name": "install",
+        "user_id": 2,
+        "timestamp": 1483228800
+    }'
+}
+
+main(){
+    COUNTER=0
+    while [  $COUNTER -lt 100000 ]; do
+        submit_crash_report
+        submit_purchase
+        submit_install
+        let COUNTER=COUNTER+1 
+    done
+}
+
+main
